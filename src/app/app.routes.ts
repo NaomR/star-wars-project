91dd93ed1c61d46da410dcc5c8f3d2b63b81b6df
Naom Ricardo <78932401+NaomR@users.noreply.github.com>
2024-03-20 12:54:59 -0300

    Day 1 - Starting project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { ShipComponent } from './pages/ship/ship.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home', component:HomeComponent},
+  {path:'films', component:FilmsComponent},
+  {path:'ship', component:ShipComponent}
+];
